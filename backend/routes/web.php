7cbf39ab969bbe46ee82127158906a4031df92bb
ankuraditya <ankuraditya1996@gmail.com>
2026-09-13 19:09:24 +0530

    Redirect admin subdomain root to CMS

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.panel');
 });
 
 Route::view('/admin/{path?}', 'admin')
